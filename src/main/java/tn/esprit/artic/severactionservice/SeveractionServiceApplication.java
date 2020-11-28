f04d2df5c352f13dd98cec46b065d7e0e5c7e040
Romaric fosso <romaricfosso6@gmail.com>
2020-11-28 10:11:56 -0800

    first commit

---
+++ src/main/java/tn/esprit/artic/severactionservice/SeveractionServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.esprit.artic.severactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeveractionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeveractionServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tn/esprit/artic/severactionservice/SeveractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.esprit.artic.severactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeveractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
